1095d0bfd85dd967af6cb8a4fc6ee4b4ef9886fc
Brian Hayes <bjoeyhayes@gmail.com>
2017-11-02 14:02:38 -0400

    add original core

---
+++ src/gingabulous.core.js
@@ -0,0 +1,49 @@
+!function() {
+var Gingabulous = {
+  modules: {},
+
+  /**
+   * Covert CamalCase to hyphenated string.
+   * @method _hyphenate
+   *
+   * Found in Foundation for Sites core file. Link to that file, and the link they have
+   * as the source for the solution.
+   * @link https://github.com/zurb/foundation-sites/blob/develop/js/foundation.core.js
+   * @link http://stackoverflow.com/a/8955580
+   *
+   * @param  {string}   string - String to parse and covert.
+   * @return {string}          - hyphenated string.
+   */
+  _hyphenate: function(string) {
+    return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+  },
+  // _getDataAttr: function(dataAttrName) {
+  //   return `data-${dataAttrName}`;
+  // },
+  registerModule: function(module, jquery = false, dataAttrName) {
+    var name = module.prototype.constructor.name;
+    // If no value for `dataAttrName` is passed, then assign `name` to the value.
+    dataAttrName = dataAttrName || this._hyphenate(name);
+    // Add the module to the `modules` object.
+    this.modules[name] = {
+      name:           name,
+      dataAttr:       `data-${dataAttrName}`,
+      dataAttrTarget: `[data-${dataAttrName}]`,
+      jquery:         jquery
+    };
+    this[name] = module;
+  }
+};
+
+Gingabulous.breakpoints = {
+  sm:  0,
+  md:  40,
+  lg:  64,
+  xl:  75,
+  xxl: 90
+};
+
+// Gingabulous.registerGlobalEventListener('scroll', window);
+
+window.Gingabulous = Gingabulous;
+}();
