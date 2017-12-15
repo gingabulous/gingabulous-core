6f34155582ca5b119e785827e0396b47c69a3007
Brian Hayes <bjoeyhayes@gmail.com>
2017-12-14 20:51:46 -0500

    add basic tests and structure

--- test/test.js
+++ test/test.js
@@ -1,1 +1,14 @@
+describe('Gingabulous core', function() {
+  it('is properly attached to the window', function() {
+    expect(window.Gingabulous).to.be.an('object');
+  });
 
+  describe('_hyphenate', function() {
+    it('returns hyphenated strings from passed camelCased strings', function() {});
+  });
+  describe('registerModule', function() {
+    afterEach(function() {
+    });
+    it('adds the module to the Gingabulous namespace', function() {});
+  });
+});
